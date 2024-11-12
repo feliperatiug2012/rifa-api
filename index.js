54a5a44875446f7892a949dcf2caf158adac1fed
Felipe Hernández <felipe.hernandez@dsintegrations.com>
2024-11-12 18:32:56 -0500

    one more time

--- index.js
+++ index.js
@@ -1,51 +1,9 @@
-import express, { json } from 'express'
-import cors from 'cors';
-import { getAllUserData, insertNewUserData } from './helpers/db.js'
-import dotenv from 'dotenv'
-import { sendMessage } from './helpers/message.js';
+const server = require('./server');
 
-dotenv.config();
+const port = process.env.PORT || 3000;
 
-const app = express();
-
-app.use(json());
-app.use(cors());
-
-app.get('/', (req, res) => {
-    res.status(200).send('API is working');
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
 
-app.get('/api/numbers', async (req, res) => {
-    const numbers = await getAllUserData();
-    res.status(200).json(numbers);
-});
-
-app.post('/api/numbers', async (req, res) => {
-    const { userName, number, state, seller } = req.body;
-
-    if (!userName || !number || !state || !seller) {
-        return res.status(400).json({ error: 'All fields are mandatory' });
-    }
-
-    const body = {
-        "user_name": userName,
-        number,
-        state,
-        seller
-    }
-    
-    try {
-        const newNumber = await insertNewUserData(body);
-        if (newNumber === null) {
-            return res.status(409).json({ error: 'Number already exists' });
-        }
-        sendMessage(seller, number);
-        res.status(200).send(newNumber);
-    } catch (err) {
-        res.status(500).send(`error inserting new number`);
-    }
-});
-
-app.listen(process.env.PORT,() => {
-    console.log(`Server Runing Port ${process.env.PORT}`);
-});
+module.exports = server;
---
+++ server.js
@@ -0,0 +1,47 @@
+import express, { json } from 'express'
+import cors from 'cors';
+import { getAllUserData, insertNewUserData } from './helpers/db.js'
+import dotenv from 'dotenv'
+import { sendMessage } from './helpers/message.js';
+
+dotenv.config();
+
+export const app = express();
+
+app.use(json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.status(200).send('API is working');
+});
+
+app.get('/api/numbers', async (req, res) => {
+    const numbers = await getAllUserData();
+    res.status(200).json(numbers);
+});
+
+app.post('/api/numbers', async (req, res) => {
+    const { userName, number, state, seller } = req.body;
+
+    if (!userName || !number || !state || !seller) {
+        return res.status(400).json({ error: 'All fields are mandatory' });
+    }
+
+    const body = {
+        "user_name": userName,
+        number,
+        state,
+        seller
+    }
+    
+    try {
+        const newNumber = await insertNewUserData(body);
+        if (newNumber === null) {
+            return res.status(409).json({ error: 'Number already exists' });
+        }
+        sendMessage(seller, number);
+        res.status(200).send(newNumber);
+    } catch (err) {
+        res.status(500).send(`error inserting new number`);
+    }
+});
